42a2920f5a0049b89896c09251d2a85e56c5398a
Vince Lavang <vincelavang@gmail.com>
2017-07-11 14:08:22 -0700

    Connect EMRnow database to knex. #1

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const jsonParser = bodyParser.json()
+
+const knex = require('knex')({
+  dialect: 'pg',
+  connection: 'postgres://localhost:5432/emrnow'
+})
