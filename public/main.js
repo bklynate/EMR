64c4c60704dea47be3cf59922d74afb73914ba94
Vince Lavang <vincelavang@gmail.com>
2017-07-14 13:21:47 -0700

    Added fetch method to render images. #2

--- public/main.js
+++ public/main.js
@@ -1,6 +1,9 @@
 const $createClient = document.querySelector('#new-client')
 const $browse = document.querySelector('#browse')
 const $save = document.querySelector('#save')
+const $results = document.querySelector('#results')
+const $clientResults = document.querySelector('#client-results')
+const $panel = document.querySelector('#panel')
 
 $createClient.addEventListener('submit', function (event) {
   event.preventDefault()
@@ -22,3 +25,44 @@ $browse.addEventListener('change', function (event) {
 $save.addEventListener('click', function (event) {
   document.querySelector('#panel').style.display = 'none'
 })
+
+fetch('/clients')
+  .then(response => {
+    console.log(response)
+    return response.json()
+  })
+  .then(users => {
+    users.map(renderUser)
+      .forEach($results => {
+        $clientResults.appendChild($results)
+      })
+  })
+
+function renderUser(client) {
+  const {first_name, last_name, intake_date, picture} = client
+  const $col = document.createElement('div')
+  $col.classList.add('col-sm-6')
+  $col.classList.add('col-md-4')
+  const $thumbnail = document.createElement('div')
+  $thumbnail.classList.add('thumbnail')
+  const $image = document.createElement('img')
+  $image.setAttribute('src', 'images/' + picture)
+  const $caption = document.createElement('div')
+  $caption.classList.add('caption')
+  const $fullname = document.createElement('h3')
+  $fullname.textContent = first_name + ' ' + last_name
+  const $date = document.createElement('p')
+  const d = new Date(intake_date)
+  const datestring = 'Intake Date:' + ' ' + d.getDate() + '-' + (d.getMonth() + 1) + '-' + d.getFullYear()
+  $date.textContent = datestring
+  console.log(intake_date)
+  console.log(new Date(intake_date))
+  $results.appendChild($col)
+  $col.appendChild($thumbnail)
+  $thumbnail.appendChild($image)
+  $thumbnail.appendChild($caption)
+  $caption.appendChild($fullname)
+  $caption.appendChild($date)
+
+  return $results
+}
