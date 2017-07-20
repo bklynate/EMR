2f37004de1ed893c238252b0df2949da3e329d75
Vince Lavang <vincelavang@gmail.com>
2017-07-19 20:36:54 -0700

    Fixed enum error. #11

--- create-table-notes.sql
+++ create-table-notes.sql
@@ -1,9 +1,10 @@
 DROP TABLE IF EXISTS notes;
 
+CREATE TYPE person AS ENUM ('doctor', 'therapist', 'case manager');
 CREATE TABLE notes (
   id          serial,
   note_date   date,
   note_text   text,
-  note_type   enum('doctor', 'therapist', 'case manager'),
+  note_type   person,
   clients_id  integer
 );
